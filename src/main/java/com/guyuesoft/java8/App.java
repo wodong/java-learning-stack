6a2c1ee3bca2eb83bc513e10f96f1304db2fcb7f
xiaodong <xiaodongzhang1986@gmail.com>
2017-11-10 17:41:41 +0800

    init project

---
+++ src/main/java/com/guyuesoft/java8/App.java
@@ -0,0 +1,13 @@
+package com.guyuesoft.java8;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
